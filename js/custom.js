796b95b063beb84b186994544ec68a520720313b
Artur Lapayeu <artur_lapayeu@epam.com>
2021-08-13 10:50:07 +0300

    fix: added tabs a11y

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,63 @@
-(function() {
+(function () {
   var burger = document.querySelector(".burger");
   var menu = document.querySelector("#" + burger.dataset.target);
-  burger.addEventListener("click", function() {
+  burger.addEventListener("click", function () {
     burger.classList.toggle("is-active");
     menu.classList.toggle("is-active");
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
+document.querySelectorAll("#nav li").forEach(function (navEl) {
+  navEl.onclick = function () {
     toggleTab(this.id, this.dataset.target);
   };
+  navEl.onfocus = function (e) {
+      toggleTab(e.currentTarget.id, e.currentTarget.dataset.target);
+      navEl.onkeydown = function (e) {
+        // end button
+        if (e.which === 35) {
+          var navEls = document.querySelectorAll("#nav li");
+          navEls[navEls.length - 1].focus();
+          e.preventDefault();
+        }
+        // home button
+        if (e.which === 36) {
+          var navEl = document.querySelector("#nav li");
+          navEl.focus();
+          e.preventDefault();
+        }
+        // left arrow button
+        if (e.which === 37) {
+          e.currentTarget.previousElementSibling.focus();
+        }
+        // right arrow button
+        if (e.which === 39) {
+          e.currentTarget.nextElementSibling.focus();
+        }
+      }
+  }
 });
 
 function toggleTab(selectedNav, targetId) {
   var navEls = document.querySelectorAll("#nav li");
 
-  navEls.forEach(function(navEl) {
+  navEls.forEach(function (navEl) {
     if (navEl.id == selectedNav) {
       navEl.classList.add("is-active");
+      navEl.setAttribute('aria-selected', "true");
+   //   navEl.setAttribute('tabindex', '0');
     } else {
       if (navEl.classList.contains("is-active")) {
         navEl.classList.remove("is-active");
+        navEl.setAttribute('aria-selected', "false");
+    //    navEl.setAttribute('tabindex', '-1');
       }
     }
   });
 
   var tabs = document.querySelectorAll(".tab-pane");
 
-  tabs.forEach(function(tab) {
+  tabs.forEach(function (tab) {
     if (tab.id == targetId) {
       tab.style.display = "block";
     } else {
